56fa93572f1369d776441cea6d0ee1a37e64d3c7
Darien Niamir <darien.niamir@gmail.com>
2024-01-15 14:20:38 -0800

    Boxplot summarized results

--- wonders_data.py
+++ wonders_data.py
@@ -109,6 +109,74 @@ class all_wonder_games():
                     self.all_players[player] = player_df
                 else:
                     self.all_players[player] = pd.concat([self.all_players[player], player_df])
+
+            # Get victor stats
+            player_df = game.raw_data[[game.victor]].T
+            if self.all_victors is None:
+                self.all_victors = player_df
+            else:
+                self.all_victors = pd.concat([self.all_victors, player_df])
             
             self.all_games[sheet_name] = game
+
+            # Get civilization stats
+            if not game.recorded_civ:
+                continue
+
+            for civ_side in game.raw_data.loc['Civ - Side', :]:
+                
+                df = game.raw_data
+                player = df.columns[df.loc['Civ - Side'] == civ_side][0]
+                
+                civ_side_df = df[[player]].T
+                if civ_side not in list(self.all_civ_sides):
+                    self.all_civ_sides[civ_side] = civ_side_df
+                else:
+                    self.all_civ_sides[civ_side] = pd.concat([self.all_civ_sides[civ_side], civ_side_df])
+
+    def __getitem__(self, name):
+        # Customize attribute access
+        if name.upper() == 'VICTORS':
+            return self.all_victors
+        if name in list(self.all_civ_sides):
+            return self.all_civ_sides[name]
+        elif name in list(self.all_players):
+            return self.all_players[name]
+        else:
+            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
         
+    def boxplot(self, *args):
+        """Create a boxplot of all inputted categories over victory point type"""
+        plt.figure(figsize=(15, 4))
+
+        # Iterate over all inputs
+        for arg_idx, df_name in enumerate(args):
+
+            df = self[df_name]
+            df = df.iloc[:, :8]
+
+            cols = list(df)
+
+            # Enumerate over all types of victory points
+            for idx, col in enumerate(cols):
+
+                y = df[col].values
+                dx = 0.2
+                x = arg_idx * dx - (len(args) - 1) * dx / 2 + np.random.normal(idx, 0.02, size=len(y))
+                plt.boxplot(y, positions=[np.mean(x)])
+
+                # Determine plot properties
+                color = 'C%i' % arg_idx
+                if idx == 0:
+                    label = df_name
+                else:
+                    label = None
+
+                plt.plot(x, y, alpha=0.2, marker='o', lw=0, ms=4, c=color, label=label)
+
+        # Adjust plot properties
+        _ = plt.xticks(np.arange(0, len(cols), 1), cols, rotation=45)
+        plt.grid(True)
+        plt.legend(loc='best')
+        plt.title('Victory Point Allocation')
+        plt.ylabel('Points')
